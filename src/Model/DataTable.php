06eba208cef0a737929250d7fa407069c5b70bf7
EdouardPlantevin <plantevin.contact@gmail.com>
2025-01-14 18:31:05 +0100

    add Model

---
+++ src/Model/DataTable.php
@@ -0,0 +1,51 @@
+<?php 
+
+namespace App\Model;
+
+class DataTable
+{
+
+
+    private array $options = [];
+    private array $attributes = [];
+
+    public function __construct(
+        private readonly ?string $id = null,
+    )
+    {}
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function setOptions(array $options): self
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    public function setAttributes(array $attributes): self
+    {
+        $this->attributes = $attributes;
+
+        return $this;
+    }
+
+    public function getAttributes(): array
+    {
+        return $this->attributes;
+    }
+
+    public function getDataController(): ?string
+    {
+        return $this->attributes['data_controller'] ?? null;
+    }
+
+}
